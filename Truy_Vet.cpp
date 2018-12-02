882cd31e38b82b1a43e2ee759899c2b8d6476e65
Nguyễn Văn Chiên <39319777+fry1999@users.noreply.github.com>
2018-12-02 09:23:51 +0700

    Update Truy_Vet.cpp

--- Truy_Vet.cpp
+++ Truy_Vet.cpp
@@ -1,222 +1,222 @@
-#include<iostream>
-#include<cmath>
-#include<vector>
-#include<algorithm>
-#include<string.h>
+#include<bits/stdc++.h>
+
+#define M 100000
 using namespace std;
 
-bool checkSumEqualS(long a, long s){
-	while(a){
-		s-= (a%10);
-		a/=10;
-	}
-	return s==0;
+bool stop = true;
+long sum;
+vector<long> Prime;
+long table[5][5];
+int x[M];
+bool checkSum(long a, long sum);
+void sieve(int sum);
+bool isDiff_0(long a);
+inline void split(int arr[5], long a);
+void init();
+bool fill_R1C1();
+bool fill_Cheo_Xuoi();
+bool fill_R2();
+bool fill_C2();
+bool fill_R3();
+bool fill_C3();
+bool fill_R4();
+bool fill_C4();
+bool fill_C5();
+bool check_Cheo_Nguoc();
+void print();
+void init(){
+	cin>>sum;
 }
-vector<int> creatAVector(long a, int n){
-	vector<int> x;
-	while(a){
-		x.push_back(a%10);
-		a/=10;
-	}
-	reverse(x.begin()+0, x.begin()+ n);
-	return x;
+bool checkSum(long a){
+	return (a%10 + (a/10)%10 + (a/100)%10 + (a/1000)%10+ a/10000) == sum;
 }
-vector<vector<int> > sieveOfEratothenes(int n, int s){
-	long m= pow(10, n);
-	bool prime1[m+1];
-	vector<vector<int> > arrayPrime;
-	memset(prime1, true, m);
-	for(long p=2; p<m; ++p){
-		if(prime1[p]){
-			if(p>=pow(10, n-1)){
-				if(checkSumEqualS(p, s))
-				arrayPrime.push_back(creatAVector(p,n));
-			}
-			for(long k=p*2; k<m; k+=p){
-				prime1[k]=false;
+void sieve(){
+	for(long p=2; p<M; ++p){
+		if(!x[p]){
+			for(long k=p*2; k<M; k+=p){
+				x[k]=1;
 			}
 		}
 	}
-	return arrayPrime;
-}
-bool checkDifferenceOf0(vector<int> a){
-	for(int i=0; i<a.size();  ++i){
-		if(a[i]==0) return false;
-	}
-	return true;
-}
-bool fillSquare(int n, vector<vector<int> > arrayPrime, int p, int k, int ans[][100]){
-	bool f= false;
-	for(int i=p; i<arrayPrime.size(); ++i){
-		if(checkDifferenceOf0(arrayPrime[i])){
-			f=true;
-			for(int z=0; z<n; z++){
-				ans[0][z]=arrayPrime[i][z];
-			}
-			break;
+	for(long i= 10000; i<M; ++i){
+		if(!x[i] && checkSum(i)){
+			Prime.push_back(i);
 		}
-	}
-	if(f==false){
-		return false;
-	}
-	f=false;
-	for(int i=k; i<arrayPrime.size(); ++i){
-		if(checkDifferenceOf0(arrayPrime[i])){
-			f=true;
-			for(int z=0; z<n; z++){
-				ans[z][0]=arrayPrime[i][z];
-			}
-			break;
-		}
-	}
-	if(f==false){
-		return false;
-	}
-	f=false;
-	for(int i=0; i<arrayPrime.size(); ++i){
-		if(arrayPrime[i][0]==ans[n-1][0] && arrayPrime[i][n-1]==ans[0][n-1]){
-			f=true;
-			for(int z=0; z<n; ++z){
-				ans[n-z-1][z]=arrayPrime[i][z];
-			}
-			break;
-		}
-	}
-	if(f==false){
-		return false;
-	}
-	f=false;
-	for(int i=0; i<arrayPrime.size(); ++i){
-		if(arrayPrime[i][0]==ans[1][0] && arrayPrime[i][n-2]==ans[1][n-2]){
-			f=true;
-			for(int z=0; z<n; ++z){
-				ans[1][z]=arrayPrime[i][z];
-			}
-			break;
-		}
-	}
-	if(f==false){
-		return false;
-	}
-	f=false;
-	for(int i=0; i<arrayPrime.size(); ++i){
-		if(arrayPrime[i][0]==ans[0][1] && arrayPrime[i][1]==ans[1][1] && arrayPrime[i][n-2]==ans[n-2][1]){
-			f=true;
-			for(int z=0; z<n; ++z){
-				ans[z][1]=arrayPrime[i][z];
-			}
-			break;
-		}
-	}
-	if(f==false){
-		return false;
-	}
-	f=false;
-	for(int i=0; i<arrayPrime.size(); ++i){
-		if(arrayPrime[i][0]==ans[2][0] && arrayPrime[i][1]==ans[2][1] && arrayPrime[i][2]==ans[2][2]){
-			f=true;
-			for(int z=0; z<n; ++z){
-				ans[2][z]=arrayPrime[i][z];
-			}
-			break;
-		}
-	}
-	if(f==false){
-		return false;
-	}
-	f=false;
-	for(int i=0; i<arrayPrime.size(); ++i){
-		if(arrayPrime[i][0]==ans[0][2] && arrayPrime[i][1]==ans[1][2] && arrayPrime[i][2]== ans[2][2]){
-			f=true;
-			for(int z=0; z<n; ++z){
-				ans[z][2]=arrayPrime[i][z];
-			}
-			break;
-		}
-	}
-	if(f==false){
-		return false;
-	}
-	f=false;
-	for(int i=0; i<arrayPrime.size(); ++i){
-		if(arrayPrime[i][0]==ans[3][0] && arrayPrime[i][1]==ans[3][1] && arrayPrime[i][2]== ans[3][2]){
-			f=true;
-			for(int z=0; z<n; ++z){
-				ans[3][z]=arrayPrime[i][z];
-			}
-			break;
-		}
-	}
-	if(f==false){
-		return false;
-	}
-	f=false;
-	for(int i=0; i<arrayPrime.size(); ++i){
-		if(arrayPrime[i][0]==ans[0][3] && arrayPrime[i][1]==ans[1][3] && arrayPrime[i][2]== ans[2][3] && arrayPrime[i][3]==ans[3][3]){
-			f=true;
-			for(int z=0; z<n; ++z){
-				ans[z][3]=arrayPrime[i][z];
-			}
-			break;
-		}
-	}
-	if(f==false){
-		return false;
-	}
-	f=false;
-	for(int i=0; i<arrayPrime.size(); ++i){
-		if(arrayPrime[i][0]==ans[0][4] && arrayPrime[i][1]==ans[1][4] && arrayPrime[i][2]== ans[2][4] && arrayPrime[i][3]==ans[3][4]){
-			f=true;
-			for(int z=0; z<n; ++z){
-				ans[z][4]=arrayPrime[i][z];
-			}
-			break;
-		}
-	}
-	if(f==false){
-		return false;
-	}
-	f=false;
-	int sum=0;
-	for(int i=0; i<n; ++i){
-		sum+=ans[i][i];
-	}
-	if(sum!=17) return false;
-	return true;
-}
-void output(vector<vector<int> >arrayPrime, int n){
-	for(int i=0; i<arrayPrime.size(); ++i){
-		for(int j=0; j<arrayPrime[i].size(); ++j){
-			cout<<arrayPrime[i][j]<<" ";
-		}
-		cout<<endl;
 	}
 }
-
-void printSquare(int ans[][100], vector<vector<int> >arrayPrime, int n){
-	int static p=0, k=0;
-	for(p=0; p<arrayPrime.size(); ++p){
-		for(k=0; k<arrayPrime.size(); ++k){
-			if(fillSquare(n, arrayPrime, p, k, ans)){
-				for(int i=0; i<n; ++i){
-					for(int j=0; j<n; ++j){
-						cout<<ans[i][j]<<" ";
+bool isDiff_0(long a){
+	return (a%10!=0) && (a/10)%10 !=0 && (a/100)%10 !=0 && (a/1000)%10 !=0 && (a/10000)!=0;
+}
+inline void split(int arr[5], long a){
+	arr[0]=a/10000;
+	arr[1]=(a/1000)%10;
+	arr[2]=(a/100)%10;
+	arr[3]=(a/10)%10;
+	arr[4]=a%10;
+}
+bool fill_R1C1(int p, int k){
+	for(int i=p; i<Prime.size(); ++i){
+		if(isDiff_0(Prime[i])){
+			int arr[5];
+			split(arr, Prime[i]);
+			for(int it=0; it<5; ++it){
+				table[0][it] = arr[it];
+			}
+			for(int j=k; j<Prime.size(); ++j){
+				if(isDiff_0(Prime[j]) && Prime[j]/10000 == table[0][0]){
+					split(arr, Prime[j]);
+					for(int jt=0; jt<5; ++jt){
+						table [jt][0]= arr[jt];
 					}
-					cout<<"\n";
+					return true;
 				}
-				return;
 			}
 		}
 	}
-	cout<<"NO finding!";
-	return ;
+	return false;
+}
+bool fill_Cheo_Xuoi(){
+	for(int i=0; i<Prime.size(); ++i){
+		if(Prime[i]/10000 == table[4][0] && Prime[i]%10 == table[0][4]){
+			int arr[5];
+			split(arr, Prime[i]);
+			for(int i=0; i<5; ++i){
+				table[5-i-1][i]= arr[i];
+			}
+			return true;
+		}
+		
+	}
+	return false;
+}
+bool fill_R2(){
+	for(int i=0; i<Prime.size(); ++i){
+		if(Prime[i]/10000 == table[1][0] && (Prime[i]/10)%10 == table[1][3]){
+			int arr[5];
+			split(arr, Prime[i]);
+			for(int i=0; i<5; ++i){
+				table[1][i]= arr[i];
+			}
+			//print();
+			return true;
+		}
+	}
+	return false;
+}
+bool fill_C2(){
+	for(int i=0; i<Prime.size(); ++i){
+		if(Prime[i]/10000 == table[0][1] && (Prime[i]/1000)%10 == table[1][1] && (Prime[i]/10)%10 == table[3][1] ){
+			int arr[5];
+			split(arr, Prime[i]);
+			for(int i=0; i<5; ++i){
+				table[i][1]= arr[i];
+			}
+			return true;
+		}
+	}
+	return false;
+}
+bool fill_C3(){
+	for(int i=0; i<Prime.size(); ++i){
+		if(Prime[i]/10000 == table[0][2] && (Prime[i]/1000)%10 == table[1][2] && (Prime[i]/100)%10 == table[2][2] ){
+			int arr[5];
+			split(arr, Prime[i]);
+			for(int i=0; i<5; ++i){
+				table[i][2]= arr[i];
+			}
+			//print();
+			return true;
+		}
+	}
+	return false;
+}
+bool fill_R3(){
+	for(int i=0; i<Prime.size(); ++i){
+		if(Prime[i]/10000 == table[2][0] && (Prime[i]/1000)%10 == table[2][1] 
+		&& (Prime[i]/100)%10 == table[2][2] ){
+			int arr[5];
+			split(arr, Prime[i]);
+			for(int i=0; i<5; ++i){
+				table[2][i]= arr[i];
+			}
+			//print();
+			return true;
+		}
+	}
+	return false;
+}
+bool fill_R4(){
+	for(int i=0; i<Prime.size(); ++i){
+		if(Prime[i]/10000 == table[3][0] && (Prime[i]/1000)%10 == table[3][1] 
+		&& (Prime[i]/100)%10 == table[3][2] ){
+			int arr[5];
+			split(arr, Prime[i]);
+			for(int i=0; i<5; ++i){
+				table[3][i]= arr[i];
+			}
+			return true;
+		}
+	}
+	return false;
+}
+bool fill_C4(){
+	for(int i=0; i<Prime.size(); ++i){
+		if(Prime[i]/10000 == table[0][3] && (Prime[i]/1000)%10 == table[1][3] 
+		&& (Prime[i]/100)%10 == table[2][3] && (Prime[i]/10)%10 == table[3][3] ){
+			int arr[5];
+			split(arr, Prime[i]);
+			for(int i=0; i<5; ++i){
+				table[i][3]= arr[i];
+			}
+			return true;
+		}
+	}
+	return false;
+}
+bool fill_C5(){
+	for(int i=0; i<Prime.size(); ++i){
+		if(Prime[i]/10000 == table[0][4] && (Prime[i]/1000)%10 == table[1][4]
+		 && (Prime[i]/100)%10 == table[2][4] && (Prime[i]/10)%10 == table[3][4] ){
+			int arr[5];
+			split(arr, Prime[i]);
+			for(int i=0; i<5; ++i){
+				table[i][4]= arr[i];
+			}
+			return true;
+		}
+	}
+	return false;
+}
+bool check_Cheo_Nguoc(){
+	int number = 0;
+	for(int i=0; i<5; ++i){
+		number = number * 10 + table[i][i];
+	}
+	if(!x[number] && checkSum(number)) return true;
+	return false;
+}
+void print(){
+	for(int i=0; i<5; ++i){
+		for(int j=0; j<5; ++j){
+			cout<<table[i][j]<<" ";
+		}
+		cout<<"\n";
+	}
+	cout<<endl;
 }
 int main(){
-	int ans[100][100];
-	vector<vector<int> > arrayPrime;
-	int n, s;
-	cin>>n>>s;
-	arrayPrime = sieveOfEratothenes(n, s);
-	printSquare(ans, arrayPrime, n);
-	//output(arrayPrime, n);
+	init();
+	sieve();
+	while(stop){
+		for(int p = 0; p<Prime.size(); ++p){
+			for(int k = 0;k<Prime.size(); ++k){
+				if(fill_R1C1(p, k) && fill_Cheo_Xuoi() && fill_R2() && fill_C2() && fill_C3()
+				 && fill_R3() && fill_R4() && fill_C4() && fill_C5() && check_Cheo_Nguoc())
+				 {
+					print();
+				}
+			}
+		}
+		stop = false;
+	}
 }
